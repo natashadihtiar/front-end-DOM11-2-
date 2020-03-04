bd984d6860693aff9b6eec6d60beb72ac6be36f0
natashadihtiar <47831860+natashadihtiar@users.noreply.github.com>
2020-03-05 00:27:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const $box = document.querySelectorAll('.box');
+const array = ['blue', 'green', 'yellow'];
+$box.forEach(box => {
+    box.addEventListener('click', function() {
+        const colours = array.shift();
+        array.push(colours);
+        this.style.background = colours;
+    }); 
+});
